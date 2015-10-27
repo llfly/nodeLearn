b0959d13fe68b05bfe5e7afd0bf3b5e75793a9e3
llfly <lflylfy@gmail.com>
2015-10-27 23:38:25 +0800

    add route

--- blog/routes/index.js
+++ blog/routes/index.js
@@ -12,13 +12,23 @@
 
 module.exports = function(app) {
   app.get('/', function (req, res) {
-    res.render('index', { title: '标注纠错' });
+    res.render('index', { title: '个人管理' });
   });
   app.get('/login', function (req, res) {
-    res.render('login', { title: '标注纠错' });
+    res.render('login', { title: '登陆' });
   });
   app.get('/reg', function (req, res) {
-    res.render('reg', { title: '标注纠错' });
+    res.render('reg', { title: '注册' });
+  });
+  app.post('/reg', function (req, res) {
+  });
+  app.get('/post',function(req,res){
+    res.render('post',{title:'发表'})
+  });
+  app.post('/post', function (req, res) {
+  });
+  app.get('/logout',function(req,res){
+    res.render('logout',{title:'退出'})
   });
 };
 
